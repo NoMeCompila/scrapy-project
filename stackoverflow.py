02133e34f1a4c55bdaff0c19c9ae1567cf71c259
NoMeCompila <cabafer1@gmail.com>
2024-05-04 20:04:12 -0300

    first spider scrapper

---
+++ stackoverflow.py
@@ -0,0 +1,31 @@
+from scrapy import Selector
+from scrapy import Field
+from scrapy import Spider
+from scrapy import Item
+from scrapy.loader import ItemLoader
+
+
+class Titulo(Item):
+    titulo = Field()
+    #descripcion = Field()
+
+
+class StackOVerflowSpider(Spider):
+    name = "mySpider"
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
+    }
+
+
+    start_urls = ['https://stackoverflow.com/questions']
+
+    def parse(self, response):
+
+        sel = Selector(response)
+        titulos = sel.xpath("//div[@id='questions']//div[@class='s-post-summary--content']")
+
+        for titulo in titulos:
+            item = ItemLoader(Titulo(), titulo)
+            #item.add_xpath('titulo', './text()')
+            item.add_xpath('titulo', './/h3//a/text()')
+            yield item.load_item()
